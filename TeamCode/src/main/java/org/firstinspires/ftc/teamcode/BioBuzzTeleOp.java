2f28239758bae6b81b6327b2c14997947c8ffac4
techiebirb <techiebirbmainacc@gmail.com>
2026-09-15 17:17:11 -0400

    Changed pinpoint code library and added pinpoint info to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BioBuzzTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BioBuzzTeleOp.java
@@ -1,26 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-
-GoBildaPinpointDriver pinpoint;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.SensorGoBildaPinpoint;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 @TeleOp(name = "Starter Drive", group = "teaching")
 public class BioBuzzTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
-
-        GoBildaPinpointDriver pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
         DcMotor frontLeft = hardwareMap.get(DcMotor.class, "front_left");
         DcMotor frontRight = hardwareMap.get(DcMotor.class, "front_right");
         DcMotor backLeft = hardwareMap.get(DcMotor.class, "back_left");
         DcMotor backRight = hardwareMap.get(DcMotor.class, "back_right");
-        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        GoBildaPinpointDriver pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
 
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.FORWARD);
@@ -36,11 +34,13 @@ public class BioBuzzTeleOp extends LinearOpMode {
         telemetry.update();
         waitForStart();
         double scale = .5;
+
         while (opModeIsActive()) {
+            Pose2D pos = pinpoint.getPosition();
+
             double drive = -gamepad1.left_stick_y;
             double strafe = gamepad1.left_stick_x;
             double turn = gamepad1.right_stick_x;
-
 
             double frontLeftPower = drive + strafe + turn;
             double frontRightPower = drive - strafe - turn;
@@ -51,6 +51,10 @@ public class BioBuzzTeleOp extends LinearOpMode {
             frontRight.setPower(frontRightPower * scale);
             backLeft.setPower(backLeftPower * scale);
             backRight.setPower(backRightPower * scale);
+
+            telemetry.addData("Position X", "%.2f", pos.getX(DistanceUnit.MM));
+            telemetry.addData("Position Y", "%.2f", pos.getY(DistanceUnit.MM));
+            telemetry.addData("Heading", "%.2f", pos.getHeading(AngleUnit.DEGREES));
 
             telemetry.addData("Drive", "%.2f", drive);
             telemetry.addData("Strafe", "%.2f", strafe);
