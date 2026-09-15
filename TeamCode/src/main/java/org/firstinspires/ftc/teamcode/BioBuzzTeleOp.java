964eecc1a32fa6253c9be8cc4b4df0b4cfd0935d
maxthegray <maximilian.i.reich@gmail.com>
2026-09-15 16:11:16 -0400

    Add BioBuzz basic drive TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BioBuzzTeleOp.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "BioBuzz: Basic Drive", group = "23209")
+public class BioBuzzTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        DcMotor backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        DcMotor backRight = hardwareMap.get(DcMotor.class, "back_right");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+            double frontLeftPower = drive + strafe + turn;
+            double frontRightPower = drive - strafe - turn;
+            double backLeftPower = drive - strafe + turn;
+            double backRightPower = drive + strafe - turn;
+
+            double scale = Math.max(1.0, Math.max(
+                    Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)),
+                    Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+
+            frontLeft.setPower(frontLeftPower / scale);
+            frontRight.setPower(frontRightPower / scale);
+            backLeft.setPower(backLeftPower / scale);
+            backRight.setPower(backRightPower / scale);
+
+            telemetry.addData("Drive", "%.2f", drive);
+            telemetry.addData("Strafe", "%.2f", strafe);
+            telemetry.addData("Turn", "%.2f", turn);
+            telemetry.update();
+        }
+    }
+}
